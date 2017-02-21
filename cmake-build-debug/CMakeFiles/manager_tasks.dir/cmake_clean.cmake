60430b04a2ac32cdeeb8941a1dfd8aa435172705
nazarhrytsiv <nazar.hrytsiv7@gmail.com>
2017-02-21 21:47:26 +0200

    Test commit

---
+++ cmake-build-debug/CMakeFiles/manager_tasks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manager_tasks.dir/main.cpp.obj"
+  "manager_tasks.pdb"
+  "manager_tasks.exe"
+  "manager_tasks.exe.manifest"
+  "libmanager_tasks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manager_tasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
